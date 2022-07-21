5400ee7a2cdaaf8726b04862a1ca3ed2f6920667
nikan <Melliissa2022>
2022-07-21 17:57:35 +0500

    я ввела переменную х

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-x=int(input("1 katet"))
-#print(x)
-y=int(input("2 katet"))
-#print(y)
-s=x*y*0,5,6
-print(s)
+x=int(input("1n"))
+print(x)
+#я ввела переменную х
+y=int(input("2n"))
+print(y)
+#я ввела переменную y
+z=int(input("3n"))
+print(z)
+#я ввела переменную z
+sum=x+y+z
+print(sum)
+#я ввела переменную sum
